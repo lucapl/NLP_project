73ac1d855004ec057aa0be9f07c3872f917d93d7
BbqGamer <korba.adam@gmail.com>
2024-06-13 00:09:11 +0200

    Add experiment for phi3

---
+++ src/experiment_phi3.py
@@ -0,0 +1,69 @@
+import datasets
+from numpy import maximum
+from summarizer import TextGenerationSummarizer
+from evaluator import evaluate_summarizer, calculate_means
+import wandb
+import random
+import string
+from wandb.sdk.data_types.trace_tree import Trace
+
+DATASET = "Samsung/samsum"
+COUNT = 100
+
+USER_INST = "<|user|>Summarize briefly the following dialogue: "
+AGENT_INST = "<|end|>\n<|assistant|>: "
+
+if __name__ == "__main__":
+    ds = datasets.load_dataset(DATASET, trust_remote_code=True)
+    assert isinstance(ds, datasets.DatasetDict)
+    trainset = ds["train"]
+    testset = ds["test"]
+    testset = testset.shuffle(seed=42).take(COUNT)
+
+    model = "microsoft/Phi-3-mini-4k-instruct"
+    template = f"{USER_INST}$dialogue{AGENT_INST}"
+    summarizer = TextGenerationSummarizer(model, string.Template(template))
+
+    random.seed(42)
+    for p in range(20):
+        run = wandb.init(project="NLP_summarization", name=model + str(p))
+
+        trainset = trainset.shuffle(seed=42)
+        number_of_shots = random.randint(1, 5)
+        shots = trainset.take(number_of_shots)
+        prompt = ""
+        for shot in shots:
+            prompt += USER_INST + shot["dialogue"]  # type: ignore
+            prompt += AGENT_INST + shot["summary"] + "\n"  # type: ignore
+
+        new_template = string.Template(prompt + template)
+
+        results, metrics = evaluate_summarizer(testset, summarizer)
+        metrics["dialogue"] = testset["dialogue"]
+        metrics["reference"] = testset["summary"]
+
+        for i in range(COUNT):
+            inputs = {"query": testset[i]["dialogue"],
+                      "reference": testset[i]["summary"],
+                      "prompt": prompt}
+
+            outputs = {}
+            outputs["prediction"] = results["prediction"][i]
+            for row in metrics:
+                outputs[row] = metrics[row][i]
+
+            span = Trace(
+                name="summarize",
+                kind="llm",  # kind can be "llm", "chain", "agent" or "tool"
+                metadata={
+                    "model_name": model,
+                },
+                inputs=inputs,
+                outputs=outputs,
+            )
+
+            span.log(name=str(p))
+
+        means = calculate_means(metrics)
+        run.summary.update(means)
+        run.finish()
